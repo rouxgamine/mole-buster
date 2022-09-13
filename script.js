2103603787279641503b026243f5a2fa633de0ef
Svetlana Moes <svetlanamoes@gmail.com>
2022-09-12 23:07:06 -0400

    Grid created

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello world!')
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.getElementById('time-left')
+const score = document.getElementById('score') 
+
+// function randomSquare() {
+//     squares.forEach(square => {
+//         square.classList.remove('mole')
+//     })
+// }
+
+// randomSquare() 
